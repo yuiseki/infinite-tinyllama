7c4bf458c1365a99aa03b3e26d00b91c4a49788c
yuiseki <yuiseki@gmail.com>
2024-03-17 12:31:12 +0900

    Update src/data.py

--- src/data.py
+++ src/data.py
@@ -1,10 +1,18 @@
 from datasets import load_dataset
 
-load_dataset("b-mc2/sql-create-context")
+# テスト用データセット
 load_dataset("burkelibbey/colors")
+
+# コーディングデータセット（英語）
+load_dataset("b-mc2/sql-create-context")
+load_dataset("sahil2801/CodeAlpaca-20k")
+load_dataset("bigcode/starcoderdata")
+
+# 日本語データセット
 load_dataset("shi3z/alpaca_cleaned_ja_json")
 load_dataset("izumi-lab/llm-japanese-dataset")
 load_dataset("hotchpotch/JQaRA")
 load_dataset("kunishou/hh-rlhf-49k-ja")
+load_dataset("kunishou/amenokaku-code-instruct")
 load_dataset("fujiki/guanaco_ja")
 load_dataset("allenai/c4", "ja")
