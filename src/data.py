0c0058fa5a25301f004cf27a0f5b8839b50c910e
yuiseki <yuiseki@gmail.com>
2024-03-17 11:53:50 +0900

    Add src/data.py

---
+++ src/data.py
@@ -0,0 +1,10 @@
+from datasets import load_dataset
+
+load_dataset("b-mc2/sql-create-context")
+load_dataset("burkelibbey/colors")
+load_dataset("shi3z/alpaca_cleaned_ja_json")
+load_dataset("izumi-lab/llm-japanese-dataset")
+load_dataset("hotchpotch/JQaRA")
+load_dataset("kunishou/hh-rlhf-49k-ja")
+load_dataset("fujiki/guanaco_ja")
+load_dataset("allenai/c4", "ja")
