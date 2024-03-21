6d53ad29572debe131d11458487e4d39a8e52701
yuiseki <yuiseki@gmail.com>
2024-03-21 13:18:33 +0900

    Update src/test2.py

--- src/test2.py
+++ src/test2.py
@@ -1,13 +1,33 @@
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import transformers
 
-tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
+model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
-base_model = AutoModelForCausalLM.from_pretrained(
-    "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    torch_dtype=torch.float16,
-    load_in_8bit=False,
-    device_map="auto",
-    trust_remote_code=True
+
+# Load the tokenizer for the specified model.
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+# Set the padding token to be the same as the end of sentence token.
+tokenizer.pad_token = tokenizer.eos_token
+
+# Define the quantization configuration for memory-efficient training.
+bnb_config = BitsAndBytesConfig(
+    # Load the model weights in 4-bit quantized format.
+    load_in_4bit=True,
+    # Specify the quantization type to use for 4-bit quantization.
+    bnb_4bit_quant_type="nf4",
+    # Specify the data type to use for computations during training.
+    bnb_4bit_compute_dtype="float16",
+    # Specify whether to use double quantization for 4-bit quantization.
+    bnb_4bit_use_double_quant=True
 )
+# Load the model from the specified model ID and apply the quantization configuration.
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    quantization_config=bnb_config,
+    device_map="auto"
+)
+# Disable cache to improve training speed.
+model.config.use_cache = False
+# Set the temperature for pretraining to 1.
+model.config.pretraining_tp = 1
