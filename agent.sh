020832faeb3bf5ef302068d1fb7c06eefa484dfe
yuiseki <yuiseki@gmail.com>
2024-03-22 13:56:28 +0900

    WIP agent.sh

--- agent.sh
+++ agent.sh
@@ -60,22 +60,53 @@ if [ $AVAILABLE_GPU_NUM -eq 0 ]; then
   exit 1
 fi
 
-# AVAILABLE_GPU_INDEX: 5 6 7 8
-CUDA_VISIBLE_DEVICES=$(echo ${AVAILABLE_GPU_INDEX[@]} | sed 's/ /,/g')
-echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+eval "$(/home/yuiseki/miniconda3/bin/conda shell.bash hook)"
+export PATH="/home/yuiseki/miniconda3/bin:$PATH"
+conda activate peft
 
 # $1 = directory of recipes
-# echo all recipes path
 RECIPE_DIR=$1
+# $2 = MAX_ALLOWED_GPU_NUM
+MAX_ALLOWED_GPU_NUM=$2
+
+# Set CUDA_VISIBLE_DEVICES as the first available GPUs based on MAX_ALLOWED_GPU_NUM
+CUDA_VISIBLE_DEVICES=""
+for i in $(seq $MAX_ALLOWED_GPU_NUM); do
+  CUDA_VISIBLE_DEVICES+="${AVAILABLE_GPU_INDEX[$i-1]}"
+  if [ $i -lt $MAX_ALLOWED_GPU_NUM ]; then
+    CUDA_VISIBLE_DEVICES+=","
+  fi
+done
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+
+# echo all recipes path
 echo "RECIPE_DIR: $RECIPE_DIR"
 RECIPE_LIST=$(ls $RECIPE_DIR)
 AVAILABLE_RECIPES=()
 for RECIPE in $RECIPE_LIST; do
-  # echo "RECIPE: $1$RECIPE"
   RECIPE_CLAIM_GPU_NUM=$(cat $1$RECIPE | yq ".train_claim_gpu_num")
-  # echo "RECIPE_CLAIM_GPU_NUM: $RECIPE_CLAIM_GPU_NUM"
+  # If RECIPE_CLAIM_GPU_NUM is greater than MAX_ALLOWED_GPU_NUM, skip
+  if [ $RECIPE_CLAIM_GPU_NUM -gt $MAX_ALLOWED_GPU_NUM ]; then
+    continue
+  fi
   if [ $RECIPE_CLAIM_GPU_NUM -le $AVAILABLE_GPU_NUM ]; then
     AVAILABLE_RECIPES+=($RECIPE)
   fi
 done
+
 echo "AVAILABLE_RECIPES: ${AVAILABLE_RECIPES[@]}"
+
+for RECIPE in $AVAILABLE_RECIPES; do
+  TARGET_MODEL_NAME=$(cat $1$RECIPE | yq ".model_name")
+  echo "TARGET_MODEL_NAME: $TARGET_MODEL_NAME"
+  # If recipe already trained, skip
+  # If recipe already trained, directory like output/TARGET_MODEL_NAME are exists
+  # If exists, skip
+  if [ -d "output/$TARGET_MODEL_NAME" ]; then
+    echo "Skip $1$RECIPE"
+    continue
+  fi
+  # If recipe not trained, train the recipe
+  echo "Train $1$RECIPE"
+  python src/train.py $1$RECIPE
+done
