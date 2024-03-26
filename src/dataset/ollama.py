6614b93b42f9eb5ac5bf4e3d7878fe7faf5c4e90
yuiseki <yuiseki@gmail.com>
2024-03-26 09:45:59 +0900

    remove src/dataset/ollama.py

--- src/dataset/ollama.py
+++
@@ -1,9 +0,0 @@
-import ollama
-
-response = ollama.chat(model='gemma:7b-instruct', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-])
-print(response['message']['content'])
